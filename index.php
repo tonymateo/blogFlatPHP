51fa79f49862b1647c767345a82b5230c125f300
tonymateo <tonymateo95@gmail.com>
2017-01-23 10:19:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //$urlActual= $_SERVER["REQUEST_URI"] + "?id=1" = ;
 //si me pasan un id muestro el detalle si no, muestro el todo
 require_once 'modelo.php';
-echo "programador 1123 ";
+echo "programador 112";
 if(isset($_GET['id'])){
 		require 'detalle.php';
 	}else{//compruebo que el id que ha escrito existe en la base de datos
