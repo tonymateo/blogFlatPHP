7b7c9ce60088f08c517f09eeb1f1852ceaabc62e
tonymateo <tonymateo95@gmail.com>
2017-01-25 13:24:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //$urlActual= $_SERVER["REQUEST_URI"] + "?id=1" = ;
 //si me pasan un id muestro el detalle si no, muestro el todo
 require_once 'modelo.php';
-echo "Webhook funcionando y con hora";
+echo "Webhook funcionando y con hora ";
 if(isset($_GET['id'])){
 		require 'detalle.php';
 	}else{//compruebo que el id que ha escrito existe en la base de datos
